0ae79d7db69869957696e11e77b17549ebe796bb
Tice28 <90235003+Tice28@users.noreply.github.com>
2023-01-27 01:27:44 -0600

    Add files via upload

---
+++ docAlphabetFrequency/src/docAlphabetFrequency/Main.java
@@ -0,0 +1,35 @@
+package docAlphabetFrequency;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		HashMap<Character, Integer> freqArray = new HashMap<>();			//Hashmap is used to hold the kvp of characters to their frequencies						
+		String filePath ="src/docAlphabetFrequency/test.txt";				//File path used to determine what file to open
+		String temp;
+
+		Scanner s = new Scanner(new File(filePath)); 						//Scanner object used to import file
+		
+		while(s.hasNext()) {
+			temp = s.nextLine();											//Temp reads in next line that is converted to a string
+			for(int i = 0;i<temp.length();i++) {
+				if(freqArray.containsKey(temp.charAt(i))) {					//Checks to see if character has already been logged
+					int tempInt = freqArray.get(temp.charAt(i))+1;
+					freqArray.replace(temp.charAt(i), tempInt);
+				} else {													//Adds character if it has not been logged
+					freqArray.put(temp.charAt(i), 1);
+				}
+			}
+			
+		}
+		System.out.println(freqArray.toString());							//Prints resulting hashmap
+	}
+
+}
